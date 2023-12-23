686cbca6747830c985ac820732d8959a138c4045
RahulsRana2636 <rahulsrana2636@gmail.com>
2023-12-23 21:21:52 +0530

    completed

--- db/config.js
+++ db/config.js
@@ -1,22 +1,3 @@
-
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// // const DatabaseURL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/dashboard';
-// dotenv.config();
-// const DatabaseURL = process.env.DATABASE_URL;
-// mongoose.connect(DatabaseURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// db.once("open", () => {
-//   console.log("Connected to the MongoDB database");
-// });
-
-// module.exports = db;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
